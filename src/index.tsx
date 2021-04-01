d07f4152107d80c6a9d8ecc023a4e643fb38f9d3
masterambi <ramzyrash@gmail.com>
2021-04-01 20:08:58 +0700

    Implement iframe

--- src/index.tsx
+++ src/index.tsx
@@ -36,10 +36,14 @@ const App = () => {
 			}
 		});
 
-		// console.log(result);
-
 		setCode(result.outputFiles[0].text);
 	};
+
+	const html = `
+		<script>
+			${code}
+		</script>
+	`;
 
 	return (
 		<div>
@@ -48,6 +52,7 @@ const App = () => {
 				<button onClick={onClick}>Submit</button>
 			</div>
 			<pre>{code}</pre>
+			<iframe sandbox="allow-scripts" srcDoc={html} />
 		</div>
 	);
 };
