c1142db37590625d40b3af21b8dcfffe1662ab7d
masterambi <ramzyrash@gmail.com>
2021-04-04 19:33:43 +0700

    Add code editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
 	const ref = useRef<any>();
+	const iframe = useRef<any>();
 	const [input, setInput] = useState('');
-	const [code, setCode] = useState('');
 
 	const startService = async () => {
 		ref.current = await esbuild.startService({
@@ -25,6 +27,8 @@ const App = () => {
 			return;
 		}
 
+		iframe.current.srcdoc = html;
+
 		const result = await ref.current.build({
 			entryPoints: ['index.js'],
 			bundle: true,
@@ -36,23 +40,39 @@ const App = () => {
 			}
 		});
 
-		setCode(result.outputFiles[0].text);
+		// setCode(result.outputFiles[0].text);
+		iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
 	};
 
 	const html = `
-		<script>
-			${code}
-		</script>
+		<html>
+			<head>
+			</head>
+			<body>
+				<div id="root"></div>
+				<script>
+					window.addEventListener('message', (event) => {
+						try {
+							eval(event.data);
+						} catch (err) {
+							const root = document.querySelector('#root');
+							root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+							console.error(err);
+						}
+					}, false);
+				</script>
+			</body>
+		</html>
 	`;
 
 	return (
 		<div>
+			<CodeEditor initialValue="const a = 1;" onChange={value => setInput(value)} />
 			<textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
 			<div>
 				<button onClick={onClick}>Submit</button>
 			</div>
-			<pre>{code}</pre>
-			<iframe sandbox="allow-scripts" srcDoc={html} />
+			<iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
 		</div>
 	);
 };
