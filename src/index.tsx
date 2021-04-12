6e120118c5c9e50dd2a652004b35903625b868bc
masterambi <ramzyrash@gmail.com>
2021-04-12 19:25:28 +0700

    Add cells redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
+// import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
 	return (
-		<div>
-			<CodeCell />
-			{/* <CodeCell /> */}
-		</div>
+		<Provider store={store}>
+			<div>
+				<TextEditor />
+			</div>
+		</Provider>
 	);
 };
 
