92073773501242f886436af3cb653c98c6e771c0
masterambi <ramzyrash@gmail.com>
2021-04-14 20:11:25 +0700

    Extracting local state

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -25,36 +25,35 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
 			const { id, content } = action.payload;
 
 			state.data[id].content = content;
-			
-			return state;
+			return;
 		case ActionType.DELETE_CELL:
 			delete state.data[action.payload];
 			state.order = state.order.filter(id => id !== action.payload);
 
-			return state;
+			return;
 		case ActionType.MOVE_CELL:
 			const { direction } = action.payload;
 			const index = state.order.findIndex(id => id === action.payload.id);
 			const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
 			if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-				return state;
+				return;
 			}
 
 			state.order[index] = state.order[targetIndex];
 			state.order[targetIndex] = action.payload.id;
 
-			return state;
+			return;
 		case ActionType.INSERT_CELL_BEFORE:
 			const cell: Cell = {
 				content: '',
 				type: action.payload.type,
 				id: randomId()
-			}
+			};
 
 			state.data[cell.id] = cell;
 
-			const foundIndex = state.order.findIndex(id => id === action.payload.id)
+			const foundIndex = state.order.findIndex(id => id === action.payload.id);
 
 			if (foundIndex < 0) {
 				state.order.push(cell.id);
@@ -66,10 +65,10 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
 		default:
 			return state;
 	}
-});
+}, initialState);
 
 const randomId = () => {
 	return Math.random().toString(36).substr(2, 5);
-}
+};
 
 export default reducer;
